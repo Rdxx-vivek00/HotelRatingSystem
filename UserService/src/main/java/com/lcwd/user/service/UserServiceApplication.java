f51cd144081592ac413499d9f5113a11afd91d7c
Vivek Kumar <vivekkumar@Viveks-MacBook-Air.local>
2026-07-01 17:41:06 +0530

    microservice call added using rest template

--- UserService/src/main/java/com/lcwd/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/lcwd/user/service/UserServiceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class UserServiceApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
--- UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
@@ -1,21 +1,32 @@
 package com.lcwd.user.service.services.impl;
 
+import com.lcwd.user.service.entities.Hotel;
+import com.lcwd.user.service.entities.Rating;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.exceptions.ResourceNotFoundException;
 import com.lcwd.user.service.repositories.UserRepository;
 import com.lcwd.user.service.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
+
     @Override
     public User saveUser(User user) {
         String randomUserId= UUID.randomUUID().toString();
@@ -30,6 +41,32 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("resource not found with the given Id"+userId));
+        User user= userRepository
+                .findById(userId)
+                .orElseThrow(()->new ResourceNotFoundException("resource not found with the given Id"+userId));
+
+//          fetch rating of the above user from rating service
+//    localhost:8083/ratings/users/e16fcece-ae42-4907-a3e6-13a458566a57
+
+         Rating[] ratingsOfUser=restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), Rating[].class);
+        log.info("{} ",ratingsOfUser);
+        List<Rating> ratings= Arrays.stream(ratingsOfUser).toList();
+
+       List<Rating> ratingList= ratings.stream().map(rating -> {
+            //api call to hotel service to get the hotel
+           //http://localhost:8082/hotels/2f654521-92ae-4163-b419-94434ceed06a
+        ResponseEntity<Hotel> forEntity=restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+        Hotel hotel= forEntity.getBody();
+        log.info("response status code :",forEntity.getStatusCode());
+            //set the hotel to rating
+
+           rating.setHotel(hotel);
+            //return the rating
+
+               return rating;
+        }).collect(Collectors.toList());
+        user.setRatings(ratingList);
+
+        return user;
     }
 }
